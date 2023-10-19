a93160f28111b1d6d4c8d921f7cdb6b7987e062b
Charles Taylor <charlestaylor95@gmail.com>
2023-10-19 17:06:04 -0400

    Addition of floats

--- src/dsl.ts
+++ src/dsl.ts
@@ -27,15 +27,15 @@ true satisfies Assert<OpParser<"/ 9 4">, 2>;
 type AddStr<A extends string, B extends string> =
   A extends `${infer A1 extends number}.${infer A2}`
   ?  B extends `${infer B1 extends number}.${infer B2}`
-    ? Align<A2, B2> extends [infer Len extends number, infer A3 extends number, infer B3 extends number]
-
-      ? `${Add<Add<A1,B1>, Divide<Add<A3, B3>,10>>}.${Remainder<Add<A3, B3>,10>}`
+    ? AddD<A2, B2> extends [infer Carry extends number, infer Dec extends number]
+      ? `${Add<Add<A1,B1>, Carry>}.${Dec}`
       : Align<A2, B2>
     : 'failed b match'
   : 'failed a match';
 
 
 true satisfies Assert<AddStr<"3.1", "4.5">, "7.6">;
+true satisfies Assert<AddStr<"0.5", "0.99">, "1.49">;
 true satisfies Assert<AddStr<"3.7", "4.5">, "8.2">;
 true satisfies Assert<AddStr<"3.71", "4.5">, "8.21">;
 
@@ -58,9 +58,28 @@ type AddF<A extends number, B extends number> =
 true satisfies Assert<AddF<3.71, 4.5>, 8.21>;
 
 type Digit = Under<10>;
-type testlen = 'abc'['length'];
+
+// Why?
+type arrayLength = ['a', 'b', 'c']['length'];
+type stringLength = 'abc'['length'];
 
 //type StrLen<foo extends string> = foo['length'];
+type AddD<A extends string, B extends string> =
+  Align<A, B> extends [infer Len extends number, infer M extends number, infer N extends number]
+  ? Add<M, N> extends infer R extends number
+    ? Compare<NumDigits<R>, Len> extends 'GT'
+      ? `${R}` extends `1${infer Rest extends number}`
+        ? [1, Rest]
+        : never
+      : [0, R]
+    : never
+  : never;
+
+true satisfies Assert<AddD<'99', '5'>, [1, 49]>
+true satisfies Assert<AddD<'5', '99'>, [1, 49]>
+true satisfies Assert<AddD<'50', '005'>, [0, 505]>
+true satisfies Assert<AddD<'10', '001'>, [0, 101]>
+  
 type Align<A extends string, B extends string> =
   [NumDigits<A>, NumDigits<B>] extends [infer M extends number, infer N extends number]
   ? Subtract<M, N> extends infer R extends number
@@ -69,7 +88,6 @@ type Align<A extends string, B extends string> =
       ? [N, ParseInt<`${A}${Repeat<R, '0'>}`>, ParseInt<B>]
       : 'failure to subtract'
   : 'failure to match numDigits';
-// 'stub' // [`${A}`,`${B}${Repeat<R, '0'>}`]
   
 
 true satisfies Assert<Align<'999', '5'>, [3, 999, 500]>
@@ -92,6 +110,17 @@ type DivRem10<A extends number> =
 true satisfies Assert<DivRem10<43>, {div: 4, rem: 3}>
 true satisfies Assert<DivRem10<3>, {div: 0, rem: 3}>
 
+type SplitLeadDigit<A extends number> =
+  `${A}` extends `${infer Lead extends Digit}${infer Rest}`
+  ? { lead: Lead, rest: Rest }
+  : never
+
+true satisfies Assert<SplitLeadDigit<.3>, { lead: 0, rest: '.3' }>;
+true satisfies Assert<SplitLeadDigit<3>, { lead: 3, rest: '' }>;
+true satisfies Assert<SplitLeadDigit<30>, { lead: 3, rest: '0' }>;
+true satisfies Assert<SplitLeadDigit<300>, { lead: 3, rest: '00' }>;
+true satisfies Assert<SplitLeadDigit<445>, { lead: 4, rest: '45' }>;
+
 type Repeat<Amount extends number, T extends string, Count extends Num = Zero, Acc extends string = ''> =
   Amount extends Length<Count>
   ? Acc
@@ -100,9 +129,5 @@ type Repeat<Amount extends number, T extends string, Count extends Num = Zero, A
 true satisfies Assert<Repeat<3, '.'>, '...'>;
 
 type Append<A extends string | number, B extends string | number> = `${A}${B}`;
-
 type IncT<Tuple extends Num> = Push<Unit, Tuple>;
-
 type Push<T, Tuple extends any[]> = [...Tuple, T];
-type Diff<A extends number, B extends number> = Or<Subtract<A, B>, Subtract<B,A>>;
-type diff = Diff<3, 3>;
